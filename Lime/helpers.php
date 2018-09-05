24b7f23ec8aed53f955ec45dae4361617ce8d582
icodechef <dengman2010@gmail.com>
2018-09-05 10:22:04 +0800

    Update helpers.php
    simple crsf

--- Lime/helpers.php
+++ Lime/helpers.php
@@ -162,3 +162,103 @@ if (! function_exists('dump')) {
         }
     }
 }
+
+if (! function_exists('csrf_token')) {
+    /**
+     * 返回令牌名称
+     *
+     * @return string
+     */
+    function csrf_token_name() {
+        return '_token';
+    }
+
+    /**
+     * 生成嵌入令牌的隐藏元素
+     *
+     * @return string
+     */
+    function csrf_field() {
+        return '<input type="hidden" name="' . csrf_token_name() . '" value="' . htmlspecialchars(csrf_token()) . '">';
+    }
+
+    /**
+     * 生成嵌入令牌的 query
+     *
+     * @return string
+     */
+    function csrf_query() {
+        return sprintf("%s=%s", csrf_token_name(), htmlspecialchars(csrf_token()));
+    }
+
+    /**
+     * 生成嵌入令牌的 path
+     *
+     * @return string
+     */
+    function csrf_path() {
+        return sprintf("%s/%s", csrf_token_name(), htmlspecialchars(csrf_token()));
+    }
+
+    /**
+     * 返回令牌
+     *
+     * 会话 ID 就是一种既简单又安全的令牌的实现方法
+     *
+     * @return string
+     */
+    function csrf_token($length = 32, $raw_output = false) {
+        $hash = hash_hmac('sha1', session_id(), session_id());
+        return substr($hash, 0, $length);
+    }
+
+    /**
+     * 自动抓取 POST 数据来检查指定令牌当前存储的安全令牌相匹配
+     *
+     * @param   string  $token
+     * @return  boolean
+     */
+    function csrf_verify() {
+        $save = csrf_token();
+
+        if (empty($save)) {
+            return false;
+        }
+
+        $name = csrf_token_name();
+
+        $token = isset($_POST[$name]) ? $_POST[$name] : (isset($_GET[$name]) ? $_GET[$name] : '');
+
+        if (empty($token)) {
+            return false;
+        }
+
+        if (csrf_equals($save, $token)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 比较两个哈希值
+     * 
+     * @param string $a
+     * @param string $b
+     * @return boolean
+     */
+    function csrf_equals($a, $b) {
+        $a_length = strlen($a);
+        if ($a_length !== strlen($b)) {
+            return false;
+        }
+
+        $result = 0;
+
+        for ($i = 0; $i < $a_length; $i++) {
+            $result |= ord($a[ $i ]) ^ ord($b[ $i ]);
+        }
+
+        return $result === 0;
+    }
+}
